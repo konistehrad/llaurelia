bc68895b19531364b9b747c8ef2692b4870f0700
Conrad Kreyling <konistehrad@gmail.com>
2015-08-23 12:32:24 -0400

    step 1

--- config.js
+++ config.js
@@ -32,22 +32,23 @@ System.config({
     "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.6.2",
     "aurelia-router": "github:aurelia/router@0.11.0",
     "aurelia-task-queue": "github:aurelia/task-queue@0.6.2",
-    "aurelia-templating": "github:aurelia/templating@0.14.1",
+    "aurelia-templating": "github:aurelia/templating@0.14.4",
     "aurelia-templating-binding": "github:aurelia/templating-binding@0.14.0",
     "aurelia-templating-resources": "github:aurelia/templating-resources@0.14.0",
     "aurelia-templating-router": "github:aurelia/templating-router@0.15.0",
+    "aurelia/bootstrapper": "github:aurelia/bootstrapper@0.16.0",
     "aurelia/fetch-client": "github:aurelia/fetch-client@0.1.1",
-    "babel": "npm:babel-core@5.8.21",
+    "babel": "npm:babel-core@5.8.22",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "bootstrap": "github:twbs/bootstrap@3.3.5",
-    "clean-css": "npm:clean-css@3.3.8",
-    "core-js": "npm:core-js@0.9.18",
+    "clean-css": "npm:clean-css@3.3.9",
+    "core-js": "npm:core-js@1.1.1",
     "css": "github:systemjs/plugin-css@0.1.13",
     "fetch": "github:github/fetch@0.9.0",
     "font-awesome": "npm:font-awesome@4.4.0",
     "github:aurelia/animator-css@0.15.0": {
       "aurelia-metadata": "github:aurelia/metadata@0.7.3",
-      "aurelia-templating": "github:aurelia/templating@0.14.1"
+      "aurelia-templating": "github:aurelia/templating@0.14.4"
     },
     "github:aurelia/binding@0.8.6": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.2",
@@ -63,7 +64,7 @@ System.config({
       "aurelia-loader-default": "github:aurelia/loader-default@0.9.5",
       "aurelia-logging-console": "github:aurelia/logging-console@0.6.2",
       "aurelia-router": "github:aurelia/router@0.11.0",
-      "aurelia-templating": "github:aurelia/templating@0.14.1",
+      "aurelia-templating": "github:aurelia/templating@0.14.4",
       "aurelia-templating-binding": "github:aurelia/templating-binding@0.14.0",
       "aurelia-templating-resources": "github:aurelia/templating-resources@0.14.0",
       "aurelia-templating-router": "github:aurelia/templating-router@0.15.0",
@@ -91,7 +92,7 @@ System.config({
       "aurelia-metadata": "github:aurelia/metadata@0.7.3",
       "aurelia-path": "github:aurelia/path@0.8.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.6.2",
-      "aurelia-templating": "github:aurelia/templating@0.14.1",
+      "aurelia-templating": "github:aurelia/templating@0.14.4",
       "core-js": "npm:core-js@0.9.18"
     },
     "github:aurelia/history-browser@0.7.0": {
@@ -130,14 +131,14 @@ System.config({
     "github:aurelia/templating-binding@0.14.0": {
       "aurelia-binding": "github:aurelia/binding@0.8.6",
       "aurelia-logging": "github:aurelia/logging@0.6.4",
-      "aurelia-templating": "github:aurelia/templating@0.14.1"
+      "aurelia-templating": "github:aurelia/templating@0.14.4"
     },
     "github:aurelia/templating-resources@0.14.0": {
       "aurelia-binding": "github:aurelia/binding@0.8.6",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.2",
       "aurelia-logging": "github:aurelia/logging@0.6.4",
       "aurelia-task-queue": "github:aurelia/task-queue@0.6.2",
-      "aurelia-templating": "github:aurelia/templating@0.14.1",
+      "aurelia-templating": "github:aurelia/templating@0.14.4",
       "core-js": "npm:core-js@0.9.18"
     },
     "github:aurelia/templating-router@0.15.0": {
@@ -145,9 +146,9 @@ System.config({
       "aurelia-metadata": "github:aurelia/metadata@0.7.3",
       "aurelia-path": "github:aurelia/path@0.8.1",
       "aurelia-router": "github:aurelia/router@0.11.0",
-      "aurelia-templating": "github:aurelia/templating@0.14.1"
+      "aurelia-templating": "github:aurelia/templating@0.14.4"
     },
-    "github:aurelia/templating@0.14.1": {
+    "github:aurelia/templating@0.14.4": {
       "aurelia-binding": "github:aurelia/binding@0.8.6",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.2",
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
@@ -162,7 +163,7 @@ System.config({
       "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.4.1"
+      "buffer": "npm:buffer@3.4.3"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
@@ -211,12 +212,12 @@ System.config({
     "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:buffer@3.4.1": {
+    "npm:buffer@3.4.3": {
       "base64-js": "npm:base64-js@0.0.8",
       "ieee754": "npm:ieee754@1.1.6",
       "is-array": "npm:is-array@1.0.1"
     },
-    "npm:clean-css@3.3.8": {
+    "npm:clean-css@3.3.9": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "commander": "npm:commander@2.8.1",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
@@ -238,6 +239,11 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@1.1.1": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
-import 'bootstrap';
-import 'bootstrap/css/bootstrap.css!';
+export class Welcome{
+    heading = 'Welcome to the Aurelia Navigation App!';
+    firstName = 'John';
+    lastName = 'Doe';
 
-export class App {
-  configureRouter(config, router){
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title:'Child Router' }
-    ]);
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
 
-    this.router = router;
-  }
+    submit(){
+        alert(`Welcome, ${this.fullName}!`);
+    }
 }
