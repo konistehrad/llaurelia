785faca460dc43f2c553ccb25f12e93b5451109c
Conrad Kreyling <konistehrad@gmail.com>
2015-08-23 14:25:55 -0400

    ever scroll, nice

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/css/bootstrap.css!';
-import 'styles/styles.css!';
 
 export class App {
   configureRouter(config, router){
--- src/cards.js
+++ src/cards.js
@@ -1,12 +1,17 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
+import {LogManager} from 'aurelia-framework';
 import 'fetch';
+
+const logger = LogManager.getLogger('cards-viewmodel');
+const linkParser = document.createElement('a');
 
 @inject(HttpClient)
 export class Cards {
     heading = 'Love Live Cards';
     cards = [];
-    nextSet = null;
+    nextSearch = '';
+    isFetching = false;
 
     constructor(http){
         http.configure(config => {
@@ -16,21 +21,54 @@ export class Cards {
         });
 
         this.http = http;
+        this.__scrolled = this.__scrolled.bind(this);
+    }
+
+    __scrolled(ev) {
+        var element = ev.target;
+        if( element.scrollHeight - element.scrollTop === element.clientHeight ) {
+            this.pullNext();
+        }
+    }
+
+    attached() {
+        document.querySelector('.page-host').addEventListener('scroll', this.__scrolled);
+    }
+
+
+    detached() {
+        document.querySelector('.page-host').removeEventListener('scroll', this.__scrolled);
     }
 
     async activate(){
+        this.__clear();
         return this.pullNext(true);
     }
 
     async pullNext(force = false) {
-        if( this.nextUrl === null && !force ) {
+        if( this.isFetching || (this.nextSearch === null && !force) ) {
             return Promise.resolve();
         }
 
-        var response = await this.http.fetch('cards');
+        var fetchme = `cards${this.nextSearch}`;
+        logger.debug(`Fetching API: ${fetchme}`);
+        this.isFetching = true;
+        var response = await this.http.fetch(fetchme);
         var json = await response.json();
-        this.nextSet = json.next;
+        if( json.next !== null ) {
+            linkParser.href = json.next;
+            this.nextSearch = linkParser.search;
+        } else {
+            this.nextSearch = null;
+        }
         this.cards.push(...json.results);
+        this.isFetching = false;
+    }
+
+    __clear() {
+        // http://stackoverflow.com/questions/1232040/how-to-empty-an-array-in-javascript
+        this.cards.length = 0; // I can't believe it
+        this.nextSearch = '';
     }
 }
 
